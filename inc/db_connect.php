047e70a58c7a957fb6fdd586517a640ece48e8a4
Mustafa Açıkyol <acikyolmustafa17@gmail.com>
2023-11-28 21:46:08 +0300

    tuesday development

--- inc/db_connect.php
+++ inc/db_connect.php
@@ -5,11 +5,16 @@ error_reporting(0);
 ob_start();
 date_default_timezone_set("europe/istanbul");
 
-
 $url 	= "http://localhost/fitlife/";
 
+$host = 'fitlife.cgax1dk8p37y.eu-north-1.rds.amazonaws.com';
+$port = '3306'; // Typically 3306 for MySQL
+$dbname = 'fitlife';
+$username = 'admin';
+$password = 'fitlife_123';
+
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=fitlife", "root", "");
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
 
     // Set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
