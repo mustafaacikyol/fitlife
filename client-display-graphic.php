50413b66382164f03bb7f3d909e7e650e59b977b
Mustafa Açıkyol <acikyolmustafa17@gmail.com>
2023-12-04 20:45:07 +0300

    monday fifth dev

--- client-display-graphic.php
+++ client-display-graphic.php
@@ -40,7 +40,7 @@
                     <canvas id="bodyFatRatioChart" style="width:100%;max-width:900px;margin-bottom:60px;"></canvas>
 
                     <script>
-                        const xValues = [1,2,3,4,5,6,7,8,9,10];
+                        const xValues = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
                         
                         // Assuming you have a variable $json_data with your JSON data
                         var json_data = <?php echo $json_data; ?>;
